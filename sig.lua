955d374f0b28d4d5dc1cc967a2341c23245fc2fc
Fresh <52613614+8software@users.noreply.github.com>
2022-04-01 14:55:40 -0300

    Update sig.lua

--- sig.lua
+++ sig.lua
@@ -13,6 +13,6 @@ TextLabel.BorderSizePixel = 0
 TextLabel.Position = UDim2.new(0.99000001, 0, 1, 0)
 TextLabel.Size = UDim2.new(0, 131, 0, 24)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "BloxEngine v0.1.12"
+TextLabel.Text = "bloxENGINE v0.1.12"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 16.000
