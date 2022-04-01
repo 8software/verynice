f945d325769227b9caec2b672c41dcde227fd73a
Fresh <52613614+8software@users.noreply.github.com>
2022-04-01 14:25:01 -0300

    Update sig.lua

--- sig.lua
+++ sig.lua
@@ -1,4 +1,5 @@
 local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
 local TextLabel = Instance.new("TextLabel")
 
 ScreenGui.Parent = game.CoreGui
@@ -9,9 +10,9 @@ TextLabel.AnchorPoint = Vector2.new(1, 1)
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
 TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0, 1.1, 0, 1.1)
+TextLabel.Position = UDim2.new(0.99000001, 0, 1, 0)
 TextLabel.Size = UDim2.new(0, 131, 0, 24)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "BloxEngine v0.1.2"
+TextLabel.Text = "BloxEngine v0.1.12"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 16.000
