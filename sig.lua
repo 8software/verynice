d67f61312a911ce5b53944a7c4dbbd137e9f4e34
Fresh <52613614+8software@users.noreply.github.com>
2022-04-09 22:39:30 -0300

    Update sig.lua

--- sig.lua
+++ sig.lua
@@ -16,3 +16,25 @@ TextLabel.Font = Enum.Font.SourceSans
 TextLabel.Text = "bloxENGINE v0.1.12"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 16.000
+
+--new
+local water = Instance.new("ScreenGui")
+local mark = Instance.new("TextLabel")
+--Properties:
+water.Name = "water"
+water.Parent = game.Workspace
+water.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+mark.Name = "mark"
+mark.Parent = water
+mark.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+mark.BackgroundTransparency = 1.000
+mark.BorderColor3 = Color3.fromRGB(0, 0, 0)
+mark.Position = UDim2.new(0.0185070951, 0, 0, 0)
+mark.Size = UDim2.new(0, 162, 0, 63)
+mark.Font = Enum.Font.Roboto
+mark.Text = "bloxENGINE v0.1.12"
+mark.TextColor3 = Color3.fromRGB(255, 255, 255)
+mark.TextSize = 14.000
+mark.TextWrapped = true
+mark.TextXAlignment = Enum.TextXAlignment.Left
